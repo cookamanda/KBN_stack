652cd358d63be1b76739168dd5fa8822eccf598b
Amanda Cook <44095918+cookamanda@users.noreply.github.com>
2023-10-24 00:02:44 -0400

    Update stacked_eta.py
    numbers are getting small, switched to mp math. It's very slow and very accurate

--- stacked_eta.py
+++ stacked_eta.py
@@ -1,66 +1,55 @@
 # =============================================================================
 # stacked_eta.py, Written by Amanda M. Cook 
 # created: 2023-10-05
-# last modified: 2023-10-05
+# last modified: 2023-10-24
 # =============================================================================
 
 
 #TODO: Prior is uniform right now --Amanda
 
-import numpy as np 
-from scipy.integrate import quad
-from scipy.stats import poisson, truncnorm 
-import math
-from scipy.optimize import newton
+import mpmath as mp
+import numpy as np
 
-
-def pois_rate_equation(lambda_, N, conf_lim = 0.95):
-    result = 0
-    for k in range(N + 1):
-        result += (lambda_ ** k) * (math.exp(-lambda_)) / math.factorial(k)
-    return result - (1-conf_lim)
-
-def solve_rate(N):
-    initial_guess = N+3*np.sqrt(N)  # You can start with an initial guess
-    rate_solution = newton(pois_rate_equation, initial_guess, args=(N,))
-    return rate_solution
+def poisson(N, mu):
+    return mp.fmul(mp.exp(mp.fneg(mu)), mp.fdiv(mp.power(mu, N), mp.factorial(N)))
 
 def prior(eta_value):
     return 1
 
 def likelihood_integrand(eta, S, N, B, F, F_err, F_2_S=3.799E-12):
-    mean_xray_fluence = np.multiply(eta,F)/F_2_S
+    mean_xray_fluence = eta*F/F_2_S
     #propagate the radio error to x-ray error
     x_ray_fluence_err = eta*F_err/F_2_S
     #defining parameters which define the truncated norm (just defines where 0
-    #is in terms of z score, not truncated from above (np.inf))
-    a_trunc, b_trunc = (0 - mean_xray_fluence ) / x_ray_fluence_err, np.inf
-    return poisson.pmf(N, mu=(S+B)) * truncnorm.pdf(S, a_trunc, b_trunc, 
-                                                    loc=mean_xray_fluence, 
-                                                    scale=x_ray_fluence_err)
+    #is in terms of z score, not truncated from above (np.inf)
+    return mp.fdiv(mp.fmul(poisson(N, (S+B)), mp.npdf(S, mu=mean_xray_fluence,
+                                       sigma=x_ray_fluence_err )),
+                   mp.nsum(lambda x: mp.npdf(x, mu=3,sigma=1), [0, mp.inf]))
 
-def integrate_likelihood(eta, N, B, F, F_err, F_2_S=3.799E-12, max_rate=10):
-    integrand = lambda S: likelihood_integrand(eta, S, N, B, F, F_err, F_2_S)
-    result, _ = quad(integrand, 0, max_rate)
+
+def integrate_likelihood(eta, N, B, F, F_err, F_2_S= 7.631465e-12):
+    result = mp.quad(lambda S: likelihood_integrand(eta, S, N, B, F, 
+                                                    F_err, F_2_S), [0, mp.inf])
     return result
 
-def stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_err_i, max_rate, F_2_S=3.799E-12):
-    log_integrals = np.zeros(len(N_i))
+def stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_err_i, F_2_S=7.631465e-12):
+    log_integrals = mp.zeros(1,len(N_i))
     for i in range(len(N_i)):
-        log_integrals[i] =  np.log(integrate_likelihood(eta, N_i[i], B_i[i],
+        log_integrals[0,i] =  mp.log(integrate_likelihood(eta, N_i[i], B_i[i],
                                                         F_i[i],F_err_i[i], 
-                                                        F_2_S, max_rate))
-    return np.add(np.log(prior(eta)), np.sum(log_integrals))
+                                                        F_2_S))
+    return mp.fadd(mp.log(prior(eta)), mp.fsum(log_integrals))
 
 def credible_region(stacked_eta_posterior, etas,  level= 0.997,
                     return_indicies=False):
     #normalize
-    normed = np.divide(np.exp(stacked_eta_posterior),
-                       np.sum(np.exp(stacked_eta_posterior)))
+    normed = mp.fdiv(mp.exp(stacked_eta_posterior),
+                       mp.sum(mp.exp(stacked_eta_posterior)))
     center_ind = np.argmax(normed)
     inc_conf = normed[int(center_ind)]
     i = 0 
     RLL = 0
+    RLL_i = 0
     #if the most likely value is not 0
     if center_ind != 0:
         #add the posterior value of test rates until you've found your lower 
@@ -86,16 +75,22 @@ def credible_region(stacked_eta_posterior, etas,  level= 0.997,
     else:
         return RLL, RUL
 
-def eta_credible_region(N_i, B_i, F_i, F_i_err, min_eta=1E2, max_eta=1E7, 
-                        F_2_S =  3.799E-12, nsamples=500, level=0.68,return_indicies = False):
-    max_rate = solve_rate(max(N_i))
-    etas = np.logspace(np.log10(min_eta), np.log10(max_eta), nsamples)
+def eta_credible_region(N_i, B_i, F_i, F_i_err, max_eta=1E7, 
+                        F_2_S = 7.631465e-12, nsamples=500, level=0.68,return_indicies = False):
+    nsamples = int(nsamples)
+    etas = np.logspace(0, np.log10(max_eta), int(nsamples)-1)
+    etas = np.concatenate((etas , [0]))
     log_posterior = np.zeros([nsamples])
     for i, eta in enumerate(etas): 
         #print(eta)
         log_posterior[i] = stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_i_err,\
-                     max_rate, F_2_S)
+                      F_2_S)
         #print(log_posterior[i])
-    RLL, RUL = credible_region(log_posterior, etas, level)                                                         
-    return RLL, RUL
+    if return_indicies:
+        RLL, RUL, RLL_i, RUL_i = credible_region(log_posterior, etas, level, return_indicies=True) 
+        return RLL, RUL, RLL_i, RUL_i
+    else:
+        RLL, RUL = credible_region(log_posterior, etas, level, return_indicies)                                                         
+        return RLL, RUL
+
 
