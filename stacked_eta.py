9bbec4c30b503c6e8d66808ec0e2656482a1d465
Amanda Cook <44095918+cookamanda@users.noreply.github.com>
2023-10-25 19:23:39 -0400

    removed a double exponential

--- stacked_eta.py
+++ stacked_eta.py
@@ -10,6 +10,10 @@
 import mpmath as mp
 import numpy as np
 
+import mpmath as mp
+import numpy as np
+from tqdm import tqdm 
+
 def poisson(N, mu):
     return mp.fmul(mp.exp(mp.fneg(mu)), mp.fdiv(mp.power(mu, N), mp.factorial(N)))
 
@@ -43,48 +47,30 @@ def stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_err_i, F_2_S=7.631465e-12):
 def credible_region(stacked_eta_posterior, etas,  level= 0.997,
                     return_indicies=False):
     #normalize
-    normed = mp.fdiv(mp.exp(stacked_eta_posterior),
-                       mp.sum(mp.exp(stacked_eta_posterior)))
-    center_ind = np.argmax(normed)
-    inc_conf = normed[int(center_ind)]
+    exponentiated = [mp.exp(float(x)) for x in  stacked_eta_posterior] 
+    normed = [mp.fdiv(mp.exp(float(x)),mp.fsum(exponentiated)) for x in exponentiated]
+    RUL_i = np.argmin(np.abs(np.subtract(np.cumsum(normed), 1-((1-level)/2))))
+    RLL_i = np.argmin(np.abs(np.subtract(np.cumsum(normed), ((1-level)/2))))
     i = 0 
-    RLL = 0
-    RLL_i = 0
-    #if the most likely value is not 0
-    if center_ind != 0:
-        #add the posterior value of test rates until you've found your lower 
-        #limit, i.e., until you've hit an integral value of conf_lim/2, or, you
-        #hit a 0 rate
-        while inc_conf < (level/2) and i <= (center_ind):
-            i+= 1
-            inc_conf += normed[int(center_ind-i)]
-    
-    
-        RLL = etas[max(int(center_ind-i),0)]
-        RLL_i = max(int(center_ind-i),0)
-    #from the lower limit, which might be 0, add rates to include in the 
-    #credible region until the integral hits conf_lim 
-    i = 1 
-    while inc_conf < level:
-        i+= 1
-        inc_conf += normed[int(center_ind+i)]
-    RUL = etas[int(center_ind+i)]
-    RUL_i = int(center_ind+i)
+    RLL = etas[RLL_i]
+    RUL = etas[RUL_i]
     if return_indicies:
         return RLL, RUL, RLL_i, RUL_i
     else:
         return RLL, RUL
 
 def eta_credible_region(N_i, B_i, F_i, F_i_err, max_eta=1E7, 
-                        F_2_S = 7.631465e-12, nsamples=500, level=0.68,return_indicies = False):
+                        F_2_S = 7.631465e-12, nsamples=500, level=0.95,return_indicies = False):
     nsamples = int(nsamples)
     etas = np.logspace(0, np.log10(max_eta), int(nsamples)-1)
-    etas = np.concatenate((etas , [0]))
-    log_posterior = np.zeros([nsamples])
-    for i, eta in enumerate(etas): 
+    etas_edges = np.logspace(0, np.log10(max_eta), int(nsamples))
+    bin_widths = np.subtract(etas_edges[1:], etas_edges[:-1])
+    log_posterior = mp.zeros(1,int(nsamples)-1)
+    print('post_calc')
+    for i, eta in tqdm(enumerate(etas)): 
         #print(eta)
-        log_posterior[i] = stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_i_err,\
-                      F_2_S)
+        log_posterior[0,i] = mp.fadd(stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_i_err,\
+                      F_2_S), mp.log(bin_widths[i]))
         #print(log_posterior[i])
     if return_indicies:
         RLL, RUL, RLL_i, RUL_i = credible_region(log_posterior, etas, level, return_indicies=True) 
@@ -92,5 +78,4 @@ def eta_credible_region(N_i, B_i, F_i, F_i_err, max_eta=1E7,
     else:
         RLL, RUL = credible_region(log_posterior, etas, level, return_indicies)                                                         
         return RLL, RUL
-
 
