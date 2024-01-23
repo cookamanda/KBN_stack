b3f1e7ff2e957c3962ff52736f2f50c737997778
Amanda Cook <44095918+cookamanda@users.noreply.github.com>
2024-01-23 13:48:16 -0500

    made it faster

--- stacked_eta.py
+++ stacked_eta.py
@@ -1,76 +1,58 @@
 # =============================================================================
 # stacked_eta.py, Written by Amanda M. Cook 
 # created: 2023-10-05
-# last modified: 2023-10-25
+# last modified: 2024-01-23
 # =============================================================================
 
 
 #TODO: Prior is uniform right now --Amanda
 
-import mpmath as mp
 import numpy as np
-from tqdm import tqdm 
+from scipy.stats import poisson, truncnorm
+from scipy.integrate import quad
+from tqdm import tqdm
+#posterior = 1/C p(η) ∑_i ∫_S_i Pois(N_i, B_i, F_i | λ = S_i) TNorm_0^∞(η F_i/(F_2_S), η F_i σ_F_i/(F_2_S)) d S_i
 
-def poisson(N, mu):
-    return mp.fmul(mp.exp(mp.fneg(mu)), mp.fdiv(mp.power(mu, N), mp.factorial(N)))
+def S_i_integrand(S_i, N_i, B_i, a_TNorm, eta, F_i, F_2_S, F_i_err):
+    log_poisson = poisson.logpmf(N_i, S_i + B_i)
+    truncnorm_loc = eta * F_i / F_2_S
+    truncnorm_scale = F_i_err * eta / F_2_S
+    log_truncnorm = truncnorm.logpdf(S_i, a_TNorm, np.inf, loc=truncnorm_loc, scale=truncnorm_scale)
+    return np.exp(np.float128(log_poisson + log_truncnorm))
 
-def prior(eta_value):
-    return 1
-
-def likelihood_integrand(eta, S, N, B, F, F_err, F_2_S=3.799E-12):
-    mean_xray_fluence = eta*F/F_2_S
-    #propagate the radio error to x-ray error
-    x_ray_fluence_err = eta*F_err/F_2_S
-    #defining parameters which define the truncated norm (just defines where 0
-    #is in terms of z score, not truncated from above (np.inf)
-    return mp.fdiv(mp.fmul(poisson(N, (S+B)), mp.npdf(S, mu=mean_xray_fluence,
-                                       sigma=x_ray_fluence_err )),
-                   mp.nsum(lambda x: mp.npdf(x, mu=3,sigma=1), [0, mp.inf]))
-
-
-def integrate_likelihood(eta, N, B, F, F_err, F_2_S= 7.631465e-12):
-    result = mp.quad(lambda S: likelihood_integrand(eta, S, N, B, F, 
-                                                    F_err, F_2_S), [0, mp.inf])
-    return result
-
-def stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_err_i, F_2_S=7.631465e-12):
-    log_integrals = mp.zeros(1,len(N_i))
-    for i in range(len(N_i)):
-        log_integrals[0,i] =  mp.log(integrate_likelihood(eta, N_i[i], B_i[i],
-                                                        F_i[i],F_err_i[i], 
-                                                        F_2_S))
-    return mp.fadd(mp.log(prior(eta)), mp.fsum(log_integrals))
-
-def credible_region(stacked_eta_posterior, etas,  level= 0.997,
-                    return_indicies=False):
-    exponentiated = [mp.exp(float(x)) for x in  stacked_eta_posterior] 
-    normed = [mp.fdiv(float(x),mp.fsum(exponentiated)) for x in exponentiated]
-    RUL_i = np.argmin(np.abs(np.subtract(np.cumsum(normed), 1-((1-level)/2))))
-    RLL_i = np.argmin(np.abs(np.subtract(np.cumsum(normed), ((1-level)/2))))
-    i = 0 
-    RLL = etas[RLL_i]
-    RUL = etas[RUL_i]
-    if return_indicies:
-        return RLL, RUL, RLL_i, RUL_i
+def integrate_S_i_function(N_i, B_i, a_TNorm, eta, F_i, F_2_S, F_i_err):
+    if isinstance(N_i, (int, float)):
+        result, _ = quad(S_i_integrand, 0, np.inf, args=(N_i, B_i, a_TNorm, eta, F_i, F_2_S, F_i_err))
+        return result
     else:
-        return RLL, RUL
+        result = np.zeros(len(N_i))
+        for i in range(len(N_i)):
+            result[i], _ = quad(S_i_integrand, 0, np.inf, args=(N_i[i], B_i[i], a_TNorm[i], eta, F_i[i], F_2_S[i], F_i_err[i]))
+        return np.sum(result)
 
-def eta_credible_region(N_i, B_i, F_i, F_i_err, max_eta=1E7, 
-                        F_2_S = 7.631465e-12, nsamples=500, level=0.95,return_indicies = False):
-    nsamples = int(nsamples)
-    etas = np.logspace(0, np.log10(max_eta), int(nsamples)-1)
-    etas_edges = np.logspace(0, np.log10(max_eta), int(nsamples))
-    bin_widths = np.subtract(etas_edges[1:], etas_edges[:-1])
-    log_posterior = mp.zeros(1,int(nsamples)-1)
-    for i, eta in tqdm(enumerate(etas)): 
-        #print(eta)
-        log_posterior[0,i] = mp.fadd(stacked_eta_log_posterior(eta, N_i, B_i, F_i, F_i_err,\
-                      F_2_S), mp.log(bin_widths[i]))
-        #print(log_posterior[i])
-    if return_indicies:
-        RLL, RUL, RLL_i, RUL_i = credible_region(log_posterior, etas, level, return_indicies=True) 
-        return RLL, RUL, RLL_i, RUL_i
-    else:
-        RLL, RUL = credible_region(log_posterior, etas, level, return_indicies)                                                         
-        return RLL, RUL
+def eta_posterior(etas, N_i, B_i, F_i, F_i_err, F_2_S, prior=None):
+    '''
+    Prior, default is flat, same length as etas
+    '''
+    posterior = np.zeros(len(etas))
+    if prior is None:
+        prior = np.linspace(1,1,len(etas))
+    for i, eta in tqdm(enumerate(etas)):
+        a_TNorm = (0 - eta*F_i/F_2_S) / (F_i_err*eta/F_2_S)
+        int_result = integrate_S_i_function(N_i, B_i, a_TNorm, eta, F_i, F_2_S, F_i_err)
+        posterior[i] = int_result*prior[i]
+    eta_edges = np.logspace(np.log10(min(etas)),np.log10(max(etas)), len(etas)+1)
+    eta_binsizes = eta_edges[1:] - eta_edges[:-1]    
+    norm = np.sum(np.multiply(posterior,eta_binsizes))
+    return np.divide(posterior,norm)
+        
 
+def credible_region(posterior, etas, level,
+                    return_indices=False, return_tolerance=False):
+    eta_edges = np.logspace(np.log10(min(etas)), np.log10(max(etas)), len(etas)+1)
+    eta_binsizes = eta_edges[1:] - eta_edges[:-1] 
+    multiplied_by_width = np.multiply(posterior, eta_binsizes)
+    eta_upper = etas[np.argmin(np.abs(np.subtract(np.cumsum(multiplied_by_width), 1-((1-level)/2))))]
+    eta_lower = etas[np.argmin(np.abs(np.subtract(np.cumsum(multiplied_by_width), (1-level)/2)))]
+    return eta_lower, eta_upper
+              
